e3e7994c03f127fcc9164b00f86a81cd05665f02
mozzam.inamdar <mozzam.inamdar@tubelightcommunications.com>
2024-06-25 15:36:14 +0530

    added get all tournaments endpoint

--- backend/controllers/tournamentCont.js
+++ backend/controllers/tournamentCont.js
@@ -1,6 +1,70 @@
-const Tournament = require("./../models/tournamentModel")
+const Tournament = require("./../models/tournamentModel");
+const User = require("./../models/userModel");
+
+exports.CreateTournament = async (req, res) => {
+  const { name, date, location, organizer } = req.body;
+
+  // Validate required fields
+  if (!name || !date || !location || !organizer) {
+    return res.status(400).json({
+      error: `Missing required field(s): ${[
+        !name && "name",
+        !date && "date",
+        !location && "location",
+        !organizer && "organizer",
+      ]
+        .filter(Boolean)
+        .join(", ")}`,
+    });
+  }
+
+  try {
+    // Check if the organizer exists
+    const existingUser = await User.findById(organizer);
+    if (!existingUser) {
+      return res.status(404).json({ error: "Organizer ID is not correct" });
+    }
+
+    const existingTournament = await Tournament.find({
+      name: name,
+      location: location,
+    });
+
+    if (existingTournament) {
+      return res.status(400).json({ error: "Tournament already exist" });
+    }
+
+    // Create a new tournament
+    const newTournament = new Tournament({
+      name,
+      date,
+      location,
+      organizer,
+    });
+
+    // Save the tournament to the database
+    await newTournament.save();
+
+    return res.status(201).json({
+      message: "Tournament created successfully",
+      result: newTournament,
+    });
+  } catch (error) {
+    // Handle any errors that occur during the process
+    return res.status(500).json({
+      error: "An error occurred while creating the tournament",
+      details: error.message,
+    });
+  }
+};
 
 
-exports.CreateTournament = async(req, res) =>{
-    res.json({msg: "perfect"})
-}
+// Get All Tournaments
+exports.getAllTournaments = async (req, res) => {
+  try {
+    allTournaments = await Tournament.find();
+    return res.status(200).json(allTournaments)
+  } catch (err) {
+    return res.status(400).json({"error": err})
+  }
+};
--- backend/routes/tournamentRoutes.js
+++ backend/routes/tournamentRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const tournamentController = require("./../controllers/tournamentCont")
 
-router.post("/tournament", tournamentController.CreateTournament)
+router.post("/tournament", tournamentController.CreateTournament).get("/tournament", tournamentController.getAllTournaments)
 
 
 module.exports = router;
