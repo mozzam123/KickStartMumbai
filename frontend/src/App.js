88a764d1c2a16b0c3794b50e479ed396ac2e3306
mozzam123 <mozzam607@gmail.com>
2024-08-26 21:49:17 +0530

    structured the folder and made changes in register css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "./Register";
-import Login from "./Login";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
